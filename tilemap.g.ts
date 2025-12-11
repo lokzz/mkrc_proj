968e4f1b85142b1e45d2bf80d8951e7c1035731d
kltester01 <kltester01@gmail.com>
2025-12-11 16:08:35 +0000

    Update tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -12,6 +12,26 @@ namespace myTiles {
     export const tile5 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -27,6 +47,25 @@ namespace myTiles {
             case "tile5":return tile5;
             case "L1_T_006":
             case "tile6":return tile6;
+            case "transparency16":return transparency16;
+            case "L2_T_001":
+            case "tile7":return tile7;
+            case "L2_T_002":
+            case "tile8":return tile8;
+            case "L2_T_003":
+            case "tile9":return tile9;
+            case "L2_T_004":
+            case "tile10":return tile10;
+            case "L3_T_001":
+            case "tile11":return tile11;
+            case "L3_T_002":
+            case "tile12":return tile12;
+            case "L3_T_003":
+            case "tile13":return tile13;
+            case "L3_T_004":
+            case "tile14":return tile14;
+            case "L3_T_005":
+            case "tile15":return tile15;
         }
         return null;
     })
